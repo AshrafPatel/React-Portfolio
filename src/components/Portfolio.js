374a7effce62ac9efd9254daa0f5b9b8530159df
Ashraf Patel <4shr4f@live.co.uk>
2020-08-08 19:13:50 -0400

    Portfolio site projects updated

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,7 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href={item.href}>
+                    <a href={item.href}  target="_blank">
                       <img src={`${item.imgurl}`} className="item-img"/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
