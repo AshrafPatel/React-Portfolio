3ec22d2684dc0dc376146e522e743c76084d366b
Ashraf Patel <4shr4f@live.co.uk>
2020-08-18 23:53:51 -0400

    Added animation

--- src/App.js
+++ src/App.js
@@ -8,12 +8,22 @@ import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 import ReactLogo from './loader.svg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  delay: 400, // values from 0 to 3000, with step 50ms
+  duration: 1500, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+})
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      loading: true
+      loading: false
     }
   }
 
