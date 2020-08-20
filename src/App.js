26986468c0e41152981e67702221f9074a9caaf4
Ashraf Patel <4shr4f@live.co.uk>
2020-08-20 00:03:13 -0400

    Added loader added animations need to implement smooth scrollign feature again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,20 @@ import Testimonials from  './components/Testimonials';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
-import ReactLogo from './loader.svg';
+import ReactLogo from './BlockLoader.svg';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+import "./App.css"
+
+AOS.init({
+  delay: 400, // values from 0 to 3000, with step 50ms
+  duration: 1500, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+});
+
 
 class App extends Component {
   constructor() {
@@ -15,21 +28,29 @@ class App extends Component {
     this.state = {
       loading: true
     }
+    this.displayLoader = this.displayLoader.bind(this)
+  }
+
+  displayLoader() {
+    this.setState({
+      loading: false
+    })
   }
 
   componentDidMount() {
+    setTimeout(this.displayLoader, 2000)
   }
 
   render() {
     return (
       this.state.loading ? 
-      <div className="loader"><img src={ReactLogo}/></div> :
-      <div className="App">
+      <div className="loader"><img src={ReactLogo}/><h2 className="loadText">Loading</h2></div> :
+      <div className="App" style={{textAlign: "left"}}>
         <Header resumeData={resumeData}/>
         <About resumeData={resumeData}/>
         <Resume resumeData={resumeData}/>
         <Portfolio resumeData={resumeData}/>
-        <Testimonials resumeData={resumeData}/>
+        {/* <Testimonials resumeData={resumeData}/> */}
         <ContactUs resumeData={resumeData}/>
         <Footer resumeData={resumeData}/>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link, animateScroll as scroll } from "react-scroll";
+
 export default class Header extends Component {
   render() {
     let resumeData = this.props.resumeData;
@@ -10,18 +12,18 @@ export default class Header extends Component {
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
             <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#about">About</a></li>
-             <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-               <li><a className="smoothscroll" href="#contact">Contact</a></li>
+               <li className="current"><Link activeClass="active" to="#home" spy={true} smooth={true} offset={-70} duration={500}>Home</Link></li>
+               <li><Link activeClass="active" to="#about" spy={true} smooth={true} offset={-70} duration={500}>About</Link></li>
+               <li><Link activeClass="active" to="#resume" spy={true} smooth={true} offset={-70} duration={500}>Resume</Link></li>
+               <li><Link activeClass="active" to="#portfolio" spy={true} smooth={true} offset={-70} duration={500}>Works</Link></li>
+               {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
+               <li><Link activeClass="active" to="#contact" spy={true} smooth={true} offset={-70} duration={500}>Contact</Link></li>
             </ul>
          </nav>
 
          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}.</h1>
+               <h1 className="responsive-headline" data-aos="fade-up">I am {resumeData.name}.</h1>
                <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
                </h3>
                <hr/>
